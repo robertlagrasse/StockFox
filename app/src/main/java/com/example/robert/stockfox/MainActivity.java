b36ea97c9e1c0049c873f0b721aa5656052148f7
Robert LaGrasse <robert.lagrasse@gmail.com>
2016-11-06 12:47:46 -0800

    MainActivity - Implemented LoaderManager.LoaderManagerCallbacks. queryTest() in onCreate is replaced by firing up a loader manager. onCreateLoader borrows the basic query logic from queryTest(). Data ends up at onLoadFinished as expected. Iterating through data produces expected results.

--- app/src/main/java/com/example/robert/stockfox/MainActivity.java
+++ app/src/main/java/com/example/robert/stockfox/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.robert.stockfox;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +17,12 @@ import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.PeriodicTask;
 import com.google.android.gms.gcm.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     Context mContext;
     Boolean networkConnected;
     Intent mServiceIntent;
+    Cursor mCursor;
+    private static final int CURSOR_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
             GcmNetworkManager.getInstance(this).schedule(Utils.buildPeriodicTask());
         } else Utils.networkToast(this);
 
-        queryTest();
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+
     }
 
     public void insertTest(){
@@ -62,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void queryTest(){
         String TAG = "queryTest()";
-        String fullURI;
 
         Cursor cursor;
         // Pull the details on the selected movie from the MovieEntry table
@@ -95,5 +100,47 @@ public class MainActivity extends AppCompatActivity {
     public void networkToast(){
         Toast.makeText(mContext, getString(R.string.network_toast), Toast.LENGTH_SHORT).show();
         Log.e("networkToast()", "network is dead!");
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args){
+        String TAG = "onCreateLoader";
+        Log.e(TAG, "onCreateLoader called");
+        // This is the database query
+        return new CursorLoader(this, DatabaseContract.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data){
+        // This is the result of the database query - pass data to where ever.
+        String TAG = "onLoadFinished()";
+        // mCursorAdapter.swapCursor(data);
+        mCursor = data;
+
+//        Stock stock = new Stock();
+//
+//        data.moveToFirst();
+//        stock.setSymbol(data.getString(data.getColumnIndex(DatabaseContract.StockTable.SYMBOL)));
+//        Log.e(TAG, "stock.getSymbol(): " + stock.getSymbol());
+//        try {
+//            while (data.moveToNext()) {
+//                stock.setSymbol(data.getString(data.getColumnIndex(DatabaseContract.StockTable.SYMBOL)));
+//                Log.e(TAG, "stock.getSymbol(): " + stock.getSymbol());
+//            }
+//        } finally {
+//            // data.close();
+//        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader){
+        String TAG = "onLoaderReset()";
+        Log.e(TAG, "onLoaderReset called");
+        // mCursorAdapter.swapCursor(null);
     }
 }
